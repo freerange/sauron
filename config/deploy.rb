644e63234108701fb8663a2931477d50596d1e4c
James Adam <james@lazyatom.com>
2011-09-09 16:07:06 +0100

    Allow other people to add accounts.

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,3 +4,14 @@ server "gofreerange.com", :app
 
 set :application, "sauron"
 set :repository,  "git@github.com:freerange/sauron.git"
+set :default_environment, {
+  "PATH" => "/var/rubies/1.9.2-p290/bin:$PATH"
+}
+
+task :update do
+  run "cd #{deploy_to} && script/update"
+end
+
+task :add_account do
+  run "cd #{deploy_to} && script/add_account '#{email}' '#{password}'"
+end
