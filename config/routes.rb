176374a650fffe28c49f38adfbb18205e90006f3
James Adam <james@lazyatom.com>
2011-09-13 17:03:10 +0100

    Set a default route.

--- config/routes.rb
+++ config/routes.rb
@@ -54,6 +54,8 @@ Sauron::Application.routes.draw do
 
   resources :message_threads
 
+  root :to => 'message_threads#index'
+
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
