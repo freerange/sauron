82edfdeada1bda03dba7b646dc111c2001cdf083
James Adam <james@lazyatom.com>
2011-09-13 18:12:24 +0100

    Use a thread for each account.

--- lib/sauron.rb
+++ lib/sauron.rb
@@ -2,27 +2,31 @@ module Sauron
   autoload :RawMessage, 'sauron/raw_message'
 
   def self.update
-    ::GmailAccount.all.each do |account|
-      begin
-        account.each_new_message do |raw_message|
-          begin
-            raw_message.import!
-          rescue => e
-            filename = "tmp/message_failures/#{account.id}-message-#{raw_message.uid}"
-            puts "Failed to import message #{raw_message.uid}"
-            puts e
+    threads = ::GmailAccount.all.map do |account|
+      Thread.new do
+        begin
+          account.each_new_message do |raw_message|
             begin
-              File.open(filename, "w") { |f| f.write raw_message.raw_string }
+              raw_message.import!
             rescue => e
-              puts "Couldn't even save file!"
+              filename = "tmp/message_failures/#{account.id}-message-#{raw_message.uid}"
+              puts "Failed to import message #{raw_message.uid}"
               puts e
-              File.open(filename, "w") { |f| f.write e.to_s }
+              begin
+                File.open(filename, "w") { |f| f.write raw_message.raw_string }
+              rescue => e
+                puts "Couldn't even save file!"
+                puts e
+                File.open(filename, "w") { |f| f.write e.to_s }
+              end
             end
           end
+        rescue Net::IMAP::NoResponseError
+          puts "Couldn't log in for #{account.email}; skipping"
         end
-      rescue Net::IMAP::NoResponseError
-        puts "Couldn't log in for #{account.email}; skipping"
       end
     end
+    threads.each { |t| t.join }
+    puts "Done import."
   end
 end
