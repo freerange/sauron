144b5f7c74d78d2ebb74743db6549790cad66005
James Adam <james@lazyatom.com>
2011-09-13 15:21:49 +0100

    Don't crash if we can't log in.

--- lib/sauron.rb
+++ lib/sauron.rb
@@ -3,13 +3,17 @@ module Sauron
 
   def self.update
     ::GmailAccount.all.each do |account|
-      account.each_new_message do |raw_message|
-        begin
-          raw_message.import!
-        rescue => e
-          puts "Failed to import message #{raw_message.uid}"
-          File.open("tmp/message_failures/message-#{raw_message.uid}", "w") { |f| f.write raw_message.raw_string }
+      begin
+        account.each_new_message do |raw_message|
+          begin
+            raw_message.import!
+          rescue => e
+            puts "Failed to import message #{raw_message.uid}"
+            File.open("tmp/message_failures/message-#{raw_message.uid}", "w") { |f| f.write raw_message.raw_string }
+          end
         end
+      rescue Net::IMAP::NoResponseError
+        puts "Couldn't log in for #{account.email}; skipping"
       end
     end
   end
