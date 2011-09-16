ba9e72e378a049b97f5b51ce0641bec8f2f6dae3
James Adam <james@lazyatom.com>
2011-09-17 00:41:04 +0100

    Friendlier contacts.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,4 +2,15 @@ class ContactsController < ApplicationController
   def show
     @contact = Contact.find(params[:id])
   end
+
+  def edit
+    @contact = Contact.find(params[:id])
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+    if @contact.update_attributes(params[:contact])
+      redirect_to contact_path(@contact)
+    end
+  end
 end
