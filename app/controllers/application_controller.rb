65e2c598bf6470cbc973ce259e86faa832579f4b
James Adam <james@lazyatom.com>
2011-09-17 11:29:01 +0100

    USERNAME is a shell variable used by zsh.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  http_basic_authenticate_with :name => ENV["USERNAME"] || 'username', :password => ENV["PASSWORD"] || 'password'
+  http_basic_authenticate_with :name => ENV["USER_NAME"] || 'username', :password => ENV["PASSWORD"] || 'password'
 end
