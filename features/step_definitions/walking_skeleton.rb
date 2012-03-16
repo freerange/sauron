dc28f8d729fd9b4f605ff0c8747fe4d78deda4e2
Chris Roos & James Adam <pairing+chris.roos+james.adam@gofreerange.com>
2012-03-16 18:31:37 +0000

    Display date and sender for each message.

--- features/step_definitions/walking_skeleton.rb
+++ features/step_definitions/walking_skeleton.rb
@@ -3,13 +3,24 @@ require Rails.root + 'test' + 'mocks' + 'mock_gmail'
 Given /^some messages exist on the server$/ do
   GmailAccount.email = 'test@example.com'
   GmailAccount.password = 'anything'
-  [Mail.new("Subject: Message one"), Mail.new("Subject: Message two")].each do |message|
+  [
+    Mail.new("Subject: Message one\nDate: 2012-05-23 12:34:45\nFrom: Dave"),
+    Mail.new("Subject: Message two\nDate: 2012-06-22 09:21:31\nFrom: Barry")
+  ].each do |message|
     MockGmail.server.accounts['test@example.com'].add_message('INBOX', message)
   end
 end
 
 Then /^they should be visible on the messages page$/ do
   visit "/"
-  assert page.has_content?("Message one")
-  assert page.has_content?("Message two")
+  within ".message" do
+    assert page.has_css? ".subject", "Message one"
+    assert page.has_css? ".date", "2012-05-23 12:34:45"
+    assert page.has_css? ".sender", "Dave"
+  end
+  within ".message" do
+    assert page.has_css? ".subject", "Message two"
+    assert page.has_css? ".date", "2012-06-22 09:21:31"
+    assert page.has_css? ".sender", "Barry"
+  end
 end
--- test/functional/messages_controller_test.rb
+++ test/functional/messages_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class MessagesControllerTest < ActionController::TestCase
   test "#index assigns messages from Gmail" do
-    messages = [Mail.new("FROM: George"), Mail.new("FROM: Bob")]
+    messages = [Mail.new("FROM: George\nDate: 2012-01-01 12:00:00"), Mail.new("FROM: Bob\nDate: 2012-01-01 12:00:00")]
     GmailAccount.email = "email"
     GmailAccount.password = "password"
     GmailAccount.stubs(:messages).with("email", "password").returns(messages)
