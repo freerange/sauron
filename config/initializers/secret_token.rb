8a71e99ae6508b3bc2a70d8ae17efa45966e89d1
Tom Ward <tom@popdog.net>
2012-03-16 14:16:25 +0000

    Add rails
    The result of running `rails new sauron` on the existing codebase

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sauron::Application.config.secret_token = 'e1057f33fd65f4d8943a243bf887222ed93a9e54ddb56d2ad30fbc7b998db422313efa8a2dc7a06fa5eacb5e47a7795675745232dc541d18ed5fb5c91cf1b349'
