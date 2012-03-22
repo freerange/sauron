b9b93f611fb77f97a643a4c8509792511198ef33
Tom Ward <tom@popdog.net>
2012-03-22 21:36:14 +0000

    MessageImporter deserves its own file

--- test/unit/message_importer_test.rb
+++ test/unit/message_importer_test.rb
@@ -1,19 +1,5 @@
 require 'test_helper'
 
-class MessageImporter
-  attr_reader :message_client
-
-  def initialize(message_client)
-    @message_client = message_client
-  end
-
-  def import_into(repository)
-    message_client.inbox_messages.each do |message|
-      repository.store(message)
-    end
-  end
-end
-
 class MessageImporterTest < ActiveSupport::TestCase
   test 'imports messages' do
     gmail_client = stub('gmail-client', inbox_messages: [:message1, :message2])
