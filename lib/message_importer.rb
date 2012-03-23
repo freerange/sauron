623566548a543023fdfa0a471f96aac0fbca4b16
Chris Roos & James Adam <pairing+chris.roos+james.adam@gofreerange.com>
2012-03-23 12:13:22 +0000

    Introduce a method to import messages given credentials.
    We need to be able to import messages from an account via a high-level
    method that's suitable for calling from (for example) a Rake task.
    
    We've put this method in `MessageImporter`, but wanted to keep the tests
    slightly separate because we weren't 100% sure that it belonged there.
    
    Having written the tests, and with the two `require`s that we've had to
    add to `message_importer.rb`, we're now confident that it doesn't belong
    in that class; we'll extract it into something else next.

--- lib/message_importer.rb
+++ lib/message_importer.rb
@@ -1,4 +1,15 @@
+require 'gmail_imap_client'
+require 'message_repository'
+
 class MessageImporter
+
+  class << self
+    def import_for(email, password)
+      imap_client = GmailImapClient.connect(email, password)
+      new(imap_client).import_into(MessageRepository.instance)
+    end
+  end
+
   attr_reader :message_client
 
   def initialize(message_client)
--- test/unit/message_importer_test.rb
+++ test/unit/message_importer_test.rb
@@ -1,5 +1,34 @@
 require 'test_helper'
 
+class MessageImporter::WhenCoordinating < ActiveSupport::TestCase
+  test 'establishes an imap connection using the given credentials' do
+    GmailImapClient.expects(:connect).with('dave@example.com', 'password')
+    MessageImporter.stubs(:new).returns(stub_everything)
+
+    MessageImporter.import_for('dave@example.com', 'password')
+  end
+
+  test 'uses the established connection for importing' do
+    gmail_client = stub('gmail-client')
+    GmailImapClient.stubs(:connect).returns(gmail_client)
+
+    MessageImporter.expects(:new).with(gmail_client).returns(stub_everything)
+    MessageImporter.import_for('whatever', 'whatever')
+  end
+
+  test 'uses the default message repository' do
+    GmailImapClient.stubs(:connect).returns(stub('gmail-client'))
+
+    message_repository = stub('message-repository')
+    importer = stub('importer')
+    MessageRepository.stubs(:instance).returns(message_repository)
+    MessageImporter.stubs(:new).returns(importer)
+    importer.expects(:import_into).with(message_repository)
+
+    MessageImporter.import_for('whatever', 'whatever')
+  end
+end
+
 class MessageImporterTest < ActiveSupport::TestCase
   test 'imports messages' do
     gmail_client = stub('gmail-client', inbox_messages: [:message1, :message2])
