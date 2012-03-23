bcc61d095c4372eb968c50df5fd17bfadbf1ab75
Chris Roos & James Adam <pairing+chris.roos+james.adam@gofreerange.com>
2012-03-23 16:53:43 +0000

    Fix the build!
    Disabling the asset pipeline broke three of our controller tests. It
    would appear that the asset pipeline rack app which is mounted makes
    Rails believe that there is a route which will point at the test
    controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Sauron::Application.routes.draw do
   root to: "messages#index"
+
+  if Rails.env.test?
+    # So that we can test arbitrary test controllers but avoid exposing this catch-all route in production
+    match ':controller(/:action(/:id))'
+  end
 end
