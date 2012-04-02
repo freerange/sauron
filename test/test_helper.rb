c443a326912afec8d35f4486f040fba173192bfe
James Mead <james@floehopper.org>
2012-04-02 15:49:36 +0100

    We don't have any fixtures at the moment.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,12 +5,6 @@ require 'mocha'
 require 'fakes/fake_gmail'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
-  #
-  # Note: You'll currently still have to declare fixtures explicitly in integration tests
-  # -- they do not yet inherit this setting
-  fixtures :all
-
   # Add more helper methods to be used by all tests here...
 end
 
