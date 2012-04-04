87e94323c87fdca4f1cc56892081a0d6f468b006
Chris Roos & James Adam <pairing+chris.roos+james.adam@gofreerange.com>
2012-04-04 10:59:33 +0100

    A more useful error message when importing a message fails.
    If the import fails, it should be easier to track down the message which prompted the failure. We see the error in the mail output of cron.

--- lib/message_importer.rb
+++ lib/message_importer.rb
@@ -1,4 +1,16 @@
 class MessageImporter
+  class ImportError < RuntimeError
+    attr_reader :uid
+
+    def initialize(uid)
+      @uid = uid
+    end
+
+    def message
+      "Failed to import message with UID=#{@uid.inspect}"
+    end
+  end
+
   attr_reader :mailbox
 
   def initialize(mailbox)
@@ -8,7 +20,11 @@ class MessageImporter
   def import_into(repository)
     mailbox.uids.each do |uid|
       unless repository.exists?(mailbox.email, uid)
-        repository.add mailbox.email, uid, mailbox.message(uid)
+        begin
+          repository.add mailbox.email, uid, mailbox.message(uid)
+        rescue => e
+          raise ImportError.new(uid)
+        end
       end
     end
   end
