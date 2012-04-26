ea8494584adb56fcf1e1e1272203c4a263653cc4
James Adam <james@lazyatom.com>
2012-04-26 11:23:01 +0100

    Remove the script to back-populate message IDs.

--- populate_historical_message_ids.rb
+++
@@ -1,8 +0,0 @@
-max_id = MessageRepository::ActiveRecordMessageIndex.maximum(:id)
-
-MessageRepository::ActiveRecordMessageIndex.where("message_id IS NULL").find_each do |record|
-  message = MessageRepository::LazyOriginalMessage.new(record.account, record.uid, CacheBackedMessageStore)
-  mail = Mail.new(message.to_s)
-  record.update_attribute(:message_id, mail.message_id)
-  puts "Updated #{record.id} of #{max_id}"
-end
