75a20eb005c729a2091adaee5494a1df2dc62eb6
Chris Roos & James Mead <pairing+chris.roos+james.mead@gofreerange.com>
2012-05-08 15:45:10 +0100

    Remove temporary script that has been run on server.

--- populate_message_hash.rb
+++
@@ -1,8 +0,0 @@
-MessageRepository::ActiveRecordMailIndex.where("message_hash IS NULL").find_each do |record|
-  hash = if record.message_id
-    Digest::SHA1.hexdigest(record.message_id)
-  else
-    Digest::SHA1.hexdigest(record.from.to_s + record.date.to_s + record.subject.to_s)
-  end
-  record.update_attribute(:message_hash, hash)
-end
