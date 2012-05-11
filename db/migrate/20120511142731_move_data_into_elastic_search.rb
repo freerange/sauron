2b418c136e7c3fce6ff82879ff16c7d8d948ec5e
Tom Ward <tom@popdog.net>
2012-05-11 17:58:09 +0100

    Apparently RestClient doesn't clean up the http connection properly
    Once a certain number of messages have been imported, an http connection limit within the OS is triggered, causing the migration to fail.  To try and counteract this, sleep between each batch (yuk!)

--- db/migrate/20120511142731_move_data_into_elastic_search.rb
+++ db/migrate/20120511142731_move_data_into_elastic_search.rb
@@ -2,27 +2,33 @@ class MoveDataIntoElasticSearch < ActiveRecord::Migration
   def up
     MessageRepository::ElasticSearchMessageIndex.new.reset!
 
-    MessageRepository::ActiveRecordMessageIndex.find_each(batch_size: 100) do |message|
+    MessageRepository::ActiveRecordMessageIndex.find_in_batches(batch_size: 100) do |messages|
       index = Tire::Index.new("sauron-#{Rails.env}")
 
-      index.store(
-        id: message.message_hash,
-        type: 'message',
-        message_id: message.message_id,
-        subject: message.subject,
-        date: message.date,
-        from: message.from,
-        mail_identifier: message.mail_identifier,
-        recipients: message.recipients
-      )
-
-      message.mail_index_records.each do |index_record|
+      messages.each do |message|
         index.store(
-          type: 'mail_import',
-          account: index_record.account,
-          uid: index_record.uid
+          id: message.message_hash,
+          type: 'message',
+          message_id: message.message_id,
+          subject: message.subject,
+          date: message.date,
+          from: message.from,
+          mail_identifier: message.mail_identifier,
+          recipients: message.recipients
         )
+
+        message.mail_index_records.each do |index_record|
+          index.store(
+            type: 'mail_import',
+            account: index_record.account,
+            uid: index_record.uid
+          )
+        end
       end
+
+      puts "Imported batch of messages #{messages.first.id} => #{messages.last.id}"
+
+      sleep 3
     end
   end
 
