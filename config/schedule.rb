a0900829c4521126f5faddb06632974d6764c7e3
Tom Ward <tom@popdog.net>
2012-05-12 11:54:02 +0100

    Re-enable the scheduled mail imports.

--- config/schedule.rb
+++ config/schedule.rb
@@ -24,6 +24,6 @@ env :MAILTO, 'everyone@gofreerange.com'
 set :job_template, nil
 job_type :lockrun_rake, "/usr/local/bin/lockrun --lockfile=:lockfile.lockrun --quiet -- bash -l -c 'cd :path && RAILS_ENV=:environment nice rake :task :output'"
 
-# every 1.minute do
-#   lockrun_rake "mail:import", lockfile: 'mail-import'
-# end
+every 1.minute do
+  lockrun_rake "mail:import", lockfile: 'mail-import'
+end
