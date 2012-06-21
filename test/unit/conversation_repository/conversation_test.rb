08836f651d067c9ff969d5511b98c7a7a4ae3c5d
James Adam <james@lazyatom.com>
2012-06-21 16:05:54 -0500

    Fix build on Travis due to differing Ruby behaviour.
    For whatever reason, when Travis switched to Ruby 1.9.3-p194 on their working
    machines, the test in `test/unit/conversation_repository_test.rb` failed[1]:
    
        1) Error:
        test_retrieves_the_most_recent_conversations_from_the_conversation_index(ConversationRepositoryTest):
        ArgumentError: wrong number of arguments(1 for 0)
            /home/vagrant/builds/freerange/sauron/test/unit/conversation_repository_test.rb:8:in `initialize'
            /home/vagrant/builds/freerange/sauron/test/unit/conversation_repository_test.rb:8:in `new'
            /home/vagrant/builds/freerange/sauron/test/unit/conversation_repository_test.rb:8:in `block in <class:ConversationRepositoryTest>'
            /home/vagrant/.rvm/gems/ruby-1.9.3-p194/gems/mocha-0.10.5/lib/mocha/integration/mini_test/version_230_to_262.rb:28:in `run'
    
    The only change that I could detect was the bump in Ruby patch-levels.
    
    I could not replicate this error locally, so I installed Ruby 1.9.3-p194 in an
    Ubuntu 11.10 32-bit VM (which is what Travis uses), and thankfully could
    replicate the issue.
    
    It looks like the test in
    `test/unit/conversation_repository/conversation_test.rb` was being loaded, and
    the fact that the test was put in the `ConversationRepository` namespace by
    re-opening the class meant that the real `ConversationRepository` constant in
    `lib/conversation_repository.rb` was never being loaded by Rails, because the
    constant had been defined.
    
    It's not clear why this was the only test that failed, because this test style
    (re-opening the application class) is used in most of the other tests.
    
    In the past we have discussed the potential issues with putting tests inside
    application classes, and relying purely on Rails' autoloading rather than
    requiring files; perhaps this is a concrete case in support of revising those
    positions?
    
    [1]: http://travis-ci.org/#!/freerange/sauron/builds/1676130

--- test/unit/conversation_repository/conversation_test.rb
+++ test/unit/conversation_repository/conversation_test.rb
@@ -1,26 +1,24 @@
 require "test_helper"
 
-class ConversationRepository
-  class ConversationTest < ActiveSupport::TestCase
-    test "uses the identifier when involved in generating URLs" do
-      identifier = "abc123"
-      conversation = Conversation.new(conversation_record_stub('record', identifier: identifier))
-      assert_equal identifier, conversation.to_param
-    end
+class ConversationRepository::ConversationTest < ActiveSupport::TestCase
+  test "uses the identifier when involved in generating URLs" do
+    identifier = "abc123"
+    conversation = ConversationRepository::Conversation.new(conversation_record_stub('record', identifier: identifier))
+    assert_equal identifier, conversation.to_param
+  end
 
-    test "returns messages ordered by most recent first" do
-      newest_message = message_stub('newest message', message_id: '1', date: 1.minute.ago)
-      oldest_message = message_stub('oldest message', message_id: '2', date: 10.days.ago)
-      oldish_message = message_stub('oldish message', message_id: '3', date: 3.days.ago)
-      messages = [newest_message, oldest_message, oldish_message]
-      message_repository = stub('message repository')
-      message_repository.stubs(:find_by_message_id).returns(*messages)
+  test "returns messages ordered by most recent first" do
+    newest_message = message_stub('newest message', message_id: '1', date: 1.minute.ago)
+    oldest_message = message_stub('oldest message', message_id: '2', date: 10.days.ago)
+    oldish_message = message_stub('oldish message', message_id: '3', date: 3.days.ago)
+    messages = [newest_message, oldest_message, oldish_message]
+    message_repository = stub('message repository')
+    message_repository.stubs(:find_by_message_id).returns(*messages)
 
-      record = conversation_record_stub('record', message_ids: ['1', '2', '3'])
+    record = conversation_record_stub('record', message_ids: ['1', '2', '3'])
 
-      conversation = Conversation.new(record, message_repository)
+    conversation = ConversationRepository::Conversation.new(record, message_repository)
 
-      assert_equal [newest_message, oldish_message, oldest_message], conversation.messages
-    end
+    assert_equal [newest_message, oldish_message, oldest_message], conversation.messages
   end
 end
